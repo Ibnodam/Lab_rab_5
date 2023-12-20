56e230671ea3f33d8944dd56be3a2d4b0523c712
Ibnodam <valijonodinaev7@gmail.com>
2023-12-20 19:00:21 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,22 @@
+﻿int a = 0;
+char[] mas = new char[10];
+for (int i = 0; i < mas.Length; i++)
+{
+    Console.WriteLine($"Введите элемент массива с индексом {i}: ");
+    mas[i] = char.Parse(Console.ReadLine());
+}
+
+for (int i = 0; i < mas.Length; i++)
+{
+    for (int j = 1; j < mas.Length; j++)
+    {
+        if (mas[i] == mas[j] && i != j)
+        {
+            Console.WriteLine($"Одинаковые элементы с индексами {i} и {j}: {mas[i]} == {mas[j]}.");
+            a = 1;
+        }
+
+    }
+
+}
+if (a == 0) Console.WriteLine("В массиве нет одинаковых элементов.");
